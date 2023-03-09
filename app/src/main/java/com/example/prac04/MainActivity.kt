287e9b90f96ec661c378f8f6d5572111700bd079
YIPZIYAN <yipziyan1@gmail.com>
2023-03-09 13:54:51 +0800

    first commit

--- app/src/main/java/com/example/prac04/MainActivity.kt
+++ app/src/main/java/com/example/prac04/MainActivity.kt
@@ -2,8 +2,6 @@ package com.example.prac04
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.Spinner
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
